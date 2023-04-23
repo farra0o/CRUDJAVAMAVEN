86316d63841894d34b98c01adc7c602906a0c11b
farra0o <82480181+farra0o@users.noreply.github.com>
2023-04-23 01:18:32 -0600

    DELETE agregado a CRUD

--- src/main/resources/static/js/Usuarios.js
+++ src/main/resources/static/js/Usuarios.js
@@ -16,20 +16,36 @@ async function cargarUsuarios(){
   });
   const usuarios = await Request.json();
 
-
-let usuariosList= "";
+  let usuariosList= "";
   for ( usuario of usuarios){
-
+  let BotonBorrar= '<a href="#" onClick="borrarUsuario('+usuario.id+')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>'
   let usuarioHTML = '<tr><td>' +usuario.id+
                     '</td><td>'+ usuario.nombre+
-                    '</td><td>'+ usuario.apellido+ 
+                    '</td><td>'+ usuario.apellido+
                     '</td><td>'+usuario.correo+
-                    '<td>'+ usuario.telefono + 
-                    '</td><td> <a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
-  
+                    '<td>'+ usuario.telefono +
+                    '</td><td>'+ BotonBorrar +'</td></tr>';
+
   usuariosList+= usuarioHTML;
 }
 
   console.log(usuarios);
   document.querySelector('#Usuarios tbody').outerHTML =usuariosList;
+}
+
+async function borrarUsuario(id){
+
+if (!confirm("Desea eliminar Usuario")){
+  location.reload();
+  return;
+}
+
+const Request = await fetch('usuarios/' +id, {
+  method: 'DELETE',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+});
+  location.reload()
 }

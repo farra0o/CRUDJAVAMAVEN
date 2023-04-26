3359675c57d1be09508de6beb345cd259399a0d1
farra0o <82480181+farra0o@users.noreply.github.com>
2023-04-26 14:36:46 -0600

    Recomit

---
+++ target/classes/static/js/Usuarios.js
@@ -0,0 +1,51 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  cargarUsuarios();
+  $('#Usuarios').DataTable();
+});
+
+async function cargarUsuarios(){
+  
+  const Request = await fetch('usuarios', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  });
+  const usuarios = await Request.json();
+
+  let usuariosList= "";
+  for ( usuario of usuarios){
+  let BotonBorrar= '<a href="#" onClick="borrarUsuario('+usuario.id+')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>'
+  let usuarioHTML = '<tr><td>' +usuario.id+
+                    '</td><td>'+ usuario.nombre+
+                    '</td><td>'+ usuario.apellido+ 
+                    '</td><td>'+usuario.correo+
+                    '<td>'+ usuario.telefono +
+                    '<td>'+ usuario.contrasena +  
+                    '</td><td>'+ BotonBorrar +'</td></tr>';
+  
+  usuariosList+= usuarioHTML;
+}
+
+  console.log(usuarios);
+  document.querySelector('#Usuarios tbody').outerHTML =usuariosList;
+}
+
+async function borrarUsuario(id){
+  
+if (!confirm("Desea eliminar Usuario")){
+  location.reload();
+  return;
+}
+
+const Request = await fetch('usuarios/' +id, {
+  method: 'DELETE',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+});
+  location.reload()
+}

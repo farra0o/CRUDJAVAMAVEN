ba532075e3c6c53ac70a442ee8f4dab57a9d2b4d
farra0o <82480181+farra0o@users.noreply.github.com>
2023-04-26 14:45:15 -0600

    AddPassVerifcation

--- src/main/resources/static/js/registro.js
+++ src/main/resources/static/js/registro.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
-
 });
 
 async function crearUsuarios(){
+
   let datos ={};
   datos.nombre = document.getElementById("txtNombre").value;
   datos.apellido = document.getElementById("txtApellido").value;
   datos.correo = document.getElementById("txtCorreo").value;
   datos.telefono = document.getElementById("txtTelefono").value;
   datos.contrasena = document.getElementById("txtContraseña").value;
+
+  let RepetirPass = document.getElementById("txtContraRepetir").value;
+
+    if (RepetirPass != datos.contrasena){
+      alert("las contraseñas no coinsiden")
+      return;
+    }
+
   const Request = await fetch('usuarios', {
     method: 'POST',
     headers: {
@@ -16,8 +24,8 @@ async function crearUsuarios(){
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(datos)
-
   });
+
   const usuarios = await Request.json();
 
 }
